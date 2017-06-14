f842a05a08a781e07fd2ea14c2ff904c85b0248d
realzero0 <realzero0@naver.com>
2017-06-14 17:48:23 +0900

    Dijkstra & DP

--- Algorithms/src/beakjun/n10217KCMTravel/Main.java
+++ Algorithms/src/beakjun/n10217KCMTravel/Main.java
@@ -4,50 +4,79 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	public static final StringBuilder POOR_KCM = new StringBuilder("Poor KCM");
+	public static final int INF = 100000000;
+	public static final int START = 1;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		Airport[][] graph = new Airport[N + 1][N + 1];
-		int[] dijkstra = new int[N + 1];
-		int[] time = new int[N + 1];
-		Arrays.fill(dijkstra, 100000000);
-		for (int x = 1; x <= N; x++) {
-			for (int y = 1; y <= N; y++) {
-				graph[x][y] = new Airport();
-			}
-		}
-		for (int i = 0; i < K; i++) {
+		int T = Integer.parseInt(st.nextToken());
+		for (int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			graph[x][y].cost = Integer.parseInt(st.nextToken());
-			graph[x][y].time = Integer.parseInt(st.nextToken());
-		}
-
-		dijkstra[1] = 0;
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (dijkstra[j] > dijkstra[i] + graph[i][j].cost) {
-					dijkstra[j] = dijkstra[i] + graph[i][j].cost;
-					time[j] += graph[i][j].time;
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			int[] dist = new int[N + 1];
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			ArrayList<ArrayList<Edge>> edges = new ArrayList<>();
+			for (int i = 0; i <= N; i++) {
+				edges.add(new ArrayList<>());
+				if (i == START) {
+					dist[i] = 0;
+				} else {
+					dist[i] = INF;
 				}
 			}
-		}
-		if (dijkstra[N] != 100000000 && dijkstra[N] < M && dijkstra[N] != 0) {
-			System.out.println(time[N]);
-		} else {
-			System.out.println("Poor KCM");
+			for (int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+				Edge e = new Edge();
+				e.num = Integer.parseInt(st.nextToken());
+				e.next = Integer.parseInt(st.nextToken());
+				e.cost = Integer.parseInt(st.nextToken());
+				e.time = Integer.parseInt(st.nextToken());
+				edges.get(e.num).add(e);
+			}
+			pq.add(new Edge(1, 0, 0));
+			while (!pq.isEmpty()) {
+				Edge e = pq.remove();
+				for (Edge edge : edges.get(e.num)) {
+					if (dist[edge.next] > e.time + edge.time && M >= e.cost + edge.cost) {
+						dist[edge.next] = e.time + edge.time;
+						// 새로 생성해서 Edge들을 넣어야 모든 경우를 전부 고려가능
+						pq.add(new Edge(edge.next, e.cost + edge.cost, dist[edge.next]));
+					}
+				}
+			}
+			if (dist[N] >= INF) {
+				System.out.println(POOR_KCM);
+			} else {
+				System.out.println(new StringBuilder().append(dist[N]));
+			}
 		}
 	}
-
 }
 
-class Airport {
+class Edge implements Comparable<Edge> {
+	int num;
+	int next;
 	int cost;
 	int time;
+
+	public Edge() {
+	}
+
+	public Edge(int num, int cost, int time) {
+		this.num = num;
+		this.cost = cost;
+		this.time = time;
+	}
+
+	@Override
+	public int compareTo(Edge o) {
+		if (this.time < o.time) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
 }
