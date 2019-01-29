1980a4ace936030d25767d9fc07442fc47bba17c
Song, Jinyoung <jinyoung.song@rakuten.com>
2019-01-29 18:41:42 +0900

    Add 2606 Virus

---
+++ Algorithms/src/beakjun/n2606virus/Main.java
@@ -0,0 +1,53 @@
+package beakjun.n2606virus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int computerCount = Integer.parseInt(br.readLine());
+        int linkedComputerCount = Integer.parseInt(br.readLine());
+
+        Map<Integer, List<Integer>> computerMap = new HashMap<>();
+        boolean[] visited = new boolean[computerCount + 1];
+
+        for (int i = 0; i < linkedComputerCount; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            computerMap.putIfAbsent(from, new ArrayList<>());
+            computerMap.putIfAbsent(to, new ArrayList<>());
+            computerMap.get(from).add(to);
+            computerMap.get(to).add(from);
+        }
+
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.add(1);
+
+        int i = 0;
+        while (!queue.isEmpty()) {
+            int start = queue.remove();
+            visited[start] = true;
+            List<Integer> endList = computerMap.get(start);
+            for (Integer end : endList) {
+                if (visited[end]) {
+                    continue;
+                }
+                i++;
+                queue.add(end);
+            }
+        }
+        System.out.println(i);
+    }
+}
